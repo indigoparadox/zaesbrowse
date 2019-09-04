595fc8a297f2dd3e816e3329ca56c39e9ab95911
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-09-03 21:05:26 -0400

    Added ifdyutil archive create inline.

--- zacreate.py
+++ zacreate.py
@@ -9,10 +9,108 @@ import datetime
 import logging
 import sys
 import codecs
-import ifdyutil.archive
 
 PIDGIN_LOG_DIR = os.path.join( os.path.expanduser( '~' ), '.purple', 'logs' )
 ERROR_NO_ARCHIVE = 1
+
+def archive_create( archive_path, key, salt=None, item_list=[], index=True ):
+
+   ''' Item list must be in the format:
+   [{'path_rel, 'contents'}] '''
+
+   # TODO: Add archive_current status update stuff.
+
+   logger = logging.getLogger( 'archive.create' )
+
+   # Generate the salt if applicable.
+   if not salt:
+      salt = Random.get_random_bytes( 160 )
+      logger.debug( 'Salt generated: {}'.format(
+         base64.b64encode( salt )
+      ) )
+
+   if index:
+      # Create the search index for the archive.
+      schema = whoosh.fields.Schema(
+         path=whoosh.fields.ID(stored=True),
+         content=whoosh.fields.TEXT
+      )
+      ix_storage = whoosh.filedb.filestore.RamStorage()
+      ix = ix_storage.create_index( schema )
+      ix_writer = ix.writer()
+   
+   # Read all of the logs and write them to the archive ZIP.
+   arcio = StringIO.StringIO()
+   total_bytes = 0
+   with zipfile.ZipFile( arcio, 'w', zipfile.ZIP_DEFLATED ) as arcz:
+      for item in item_list:
+         # Make sure everything is in unicode, first.
+         if isinstance( item['path_rel'], str ):
+            item['path_rel'] = item['path_rel'].decode( 'ascii' )
+         if isinstance( item['contents'], str ):
+            item['contents'] = item['contents'].decode( 'ascii' )
+
+         # Replace special characters with entities.
+         item['path_rel'] = \
+            item['path_rel'].encode( 'ascii', errors='xmlcharrefreplace' )
+         item['path_rel'] = item['path_rel'].decode( 'ascii' )
+         item['contents'] = \
+            item['contents'].encode( 'ascii', errors='xmlcharrefreplace' )
+         item['contents'] = item['contents'].decode( 'ascii' )
+
+         # Index the item if applicable.
+         if index:
+            logger.info( 'Indexing {}...'.format( item['path_rel'] ) )
+            ix_writer.add_document(
+               path=item['path_rel'][1:],
+               content=item['contents']
+            )
+
+         # Store the item.
+         logger.info( 'Storing {}...'.format( item['path_rel'] ) )
+         arcz.writestr(
+            item['path_rel'].decode( 'ascii' ),
+            item['contents'].decode( 'ascii' )
+         )
+         total_bytes += len( item['contents'] )
+
+      if index:
+         # Write the search index to the ZIP.
+         ix_writer.commit()
+         for ix_file_name in ix_storage.list():
+            with ix_storage.open_file( ix_file_name ) as ix_file:
+               logger.info( 'Storing {}...'.format( ix_file_name ) )
+               ix_contents = ix_file.read()
+               arcz.writestr(
+                  os.path.join( '/index', ix_file_name ),
+                  ix_contents
+               )
+               total_bytes += len( ix_contents )
+
+   logger.info( 'Stored {} bytes.'.format( total_bytes ) )
+
+   # Setup the encryptor. Expand and set the key.
+   iv = Random.get_random_bytes( 16 )
+   key_crypt = pbkdf2.PBKDF2( key, salt ).read( 32 )
+   encryptor = AES.new( key_crypt, AES.MODE_CBC, iv )
+
+   # Open the output file and start writing.
+   arcio.seek( 0, os.SEEK_SET )
+   with open( archive_path, 'wb' ) as archive_file:
+      archive_file.write( VERSIONS[len( VERSIONS ) - 1] )
+      archive_file.write( salt )
+      archive_file.write( struct.pack( '<Q', len( arcio.getvalue() ) ) )
+      archive_file.write( iv )
+      # Process each chunk of the ZIP.
+      while True:
+         chunk = arcio.read( CHUNK_LEN )
+         if len( chunk ) == 0:
+            # We're done!
+            break
+         elif 0 != len( chunk ) % 16:
+            # Handle the trailing end.
+            chunk += ' ' * (16 - len( chunk ) % 16)
+         archive_file.write( encryptor.encrypt( chunk ) )
 
 def list_logs(
    protocols_accounts, date_start=None, date_end=None, log_dir=PIDGIN_LOG_DIR
@@ -143,7 +241,7 @@ def main():
       _parse_date_arg( args.end_date ),
       args.log_dir
    )
-   ifdyutil.archive.create(
+   archive_create(
       args.archive_path,
       key,
       item_list=log_list
